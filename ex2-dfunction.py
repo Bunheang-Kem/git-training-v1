f9c4e332c25a64ba3072658273b3d23cfbe65d20
BunheangKem <bkem@paragoniu.edu.kh>
2024-10-20 00:33:10 +0700

    8th commit & modified ex2 file

--- ex2-dfunction.py
+++ ex2-dfunction.py
@@ -1,3 +1,5 @@
 print("The program ran successfully")
 print("Test with code editor")
 print("Modification for 6th real commit")
+# -------------------------------------
+print("Modification for the 8th commit")
