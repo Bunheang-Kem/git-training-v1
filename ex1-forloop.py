ad7d26949738abe1964a348f02b26e0b6400a0c8
BunheangKem <bkem@paragoniu.edu.kh>
2024-10-20 00:31:58 +0700

    7th commit & modified ex1 file

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -1,3 +1,5 @@
 print("The first modification for 2nd commit")
 print("Modification for the 4th commit")
 print("Test mofidication")
+# -------------------------------------
+print("Modification for the 7th commit")
