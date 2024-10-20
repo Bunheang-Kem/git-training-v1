ff99aba2effa2633ca8068537c05299ff9f7c860
bunheangpersonal <kembunheang@gmail.com>
2024-10-20 14:51:03 +0700

    Commit from new author bunheangpersonal

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -3,3 +3,4 @@ print("Modification for the 4th commit")
 print("Test mofidication")
 # -------------------------------------
 print("Modification for the 7th commit")
+print("Add this line of code from bunheangpersonal from git terminal")
