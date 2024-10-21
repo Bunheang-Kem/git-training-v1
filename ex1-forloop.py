6c4ecf8adf1c6a5d03002eeda94bb7a365d52ab8
BunheangKem <100821642+Bunheang-Kem@users.noreply.github.com>
2024-10-21 23:17:50 +0700

    Updated a file ex1 from techteam1 on GitHub

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -4,3 +4,5 @@ print("Test mofidication")
 # -------------------------------------
 print("Modification for the 7th commit")
 print("Add this line of code from bunheangpersonal from git terminal")
+# ---------------------------------------
+print("Added 1st line from techteam1 on GitHub")
