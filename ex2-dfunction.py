385f08682823ec3249f0b2568ae8e2240456b766
BunheangKem <155383781+BunheangKem@users.noreply.github.com>
2024-10-22 01:25:14 +0700

    Updated file ex2 with B=100 & C=200

--- ex2-dfunction.py
+++ ex2-dfunction.py
@@ -7,3 +7,6 @@ print("Modification for the 8th commit")
 print("Added this line from the GitHub website for the first time")
 print("Added this line from User-2 account")
 y = 200
+print("Added this line from User-2 account")
+B = 100
+C = 200
